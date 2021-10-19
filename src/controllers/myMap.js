ccdd5e8982245bbb5fc7a8fbe63163d4d4813047
Fidomivalua <sidonie.moulart@gmail.com>
2021-10-19 15:36:44 +0200

    Controller to create my Map with survey's results

---
+++ src/controllers/myMap.js
@@ -0,0 +1,46 @@
+import survey from "../models/survey";
+import departementData from "../models/departements";
+import { securityResults, environmentResults, familyResults } from "./maps";
+import { securityWeights, environmentWeights, familyWeights } from "./weights";
+
+export const getMyMap = (a0, a1, a2, a3, a4, a5, a6, a7, a8) => {
+    const [famWeight1, famWeight2, famWeight3] = familyWeights(a0, a1, a2);
+    const [secWeight1, secWeight2, secWeight3] = securityWeights(a3, a4, a5);
+    const [envWeight1, envWeight2, envWeight3] = environmentWeights(a6, a7, a8);
+    const family_results = familyResults(famWeight1, famWeight2, famWeight3);
+    const security_results = securityResults(
+        secWeight1,
+        secWeight2,
+        secWeight3
+    );
+    const environment_results = environmentResults(
+        envWeight1,
+        envWeight2,
+        envWeight3
+    );
+
+    const result = departementData.map((element) => {
+        const family = family_results.find(
+            (ele) => ele.dep_id === element.num_dep
+        );
+        const security = security_results.find(
+            (ele) => ele.dep_id === element.num_dep
+        );
+        const environment = environment_results.find(
+            (ele) => ele.dep_id === element.num_dep
+        );
+        return {
+            dep_id: element.num_dep,
+            dep_name: element.dep_name,
+            result: (
+                (parseFloat(environment.environmental_health) +
+                    parseFloat(family.daily_life_confort) +
+                    parseFloat(security.danger_total)) /
+                3
+            ).toFixed(3),
+        };
+    });
+    result.sort((a, b) => a.result - b.result).reverse();
+
+    return result;
+};
