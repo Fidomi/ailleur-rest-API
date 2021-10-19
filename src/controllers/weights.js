df9075ebb7ed33ff890cf89ab76e97f37eb05a98
Fidomivalua <sidonie.moulart@gmail.com>
2021-10-19 15:37:30 +0200

    Weights to personalize the maps

---
+++ src/controllers/weights.js
@@ -0,0 +1,140 @@
+export const familyWeights = (a0, a1, a2) => {
+    let [weight3, weight2, weight1] = [1, 1, 1];
+    switch (a0) {
+        case "0":
+            weight3 = 1;
+            break;
+        case "1":
+            weight3 = 5;
+            break;
+        case "2":
+            weight3 = 5;
+            break;
+        default:
+            weight3 = 1;
+    }
+    switch (a1) {
+        case "0":
+            weight2 = 1;
+            break;
+        case "1":
+            weight2 = 5;
+            break;
+        case "2":
+            weight2 = 5;
+            break;
+        case "3":
+            weight2 = 1;
+            break;
+        default:
+            weight2 = 2;
+    }
+    switch (a2) {
+        case "0":
+            weight1 = 1;
+            break;
+        case "1":
+            weight1 = 2;
+            break;
+        case "2":
+            weight1 = 5;
+            break;
+        default:
+            weight1 = 2;
+    }
+    return [weight1, weight2, weight3];
+};
+
+export const securityWeights = (a3, a4, a5) => {
+    let [weight3, weight2, weight1] = [1, 1, 1];
+    switch (a3) {
+        case "0":
+            weight2 = 1;
+            break;
+        case "1":
+            weight2 = 3;
+            break;
+        case "2":
+            weight2 = 5;
+            break;
+        default:
+            weight2 = 1;
+    }
+    switch (a4) {
+        case "0":
+            weight3 = 6;
+            break;
+        case "1":
+            weight3 = 5;
+            break;
+        case "2":
+            weight3 = 2;
+            break;
+        case "3":
+            weight3 = 1;
+            break;
+        default:
+            weight3 = 1;
+    }
+    switch (a5) {
+        case "0":
+            weight1 = 1;
+            break;
+        case "1":
+            weight1 = 3;
+            break;
+        case "2":
+            weight1 = 5;
+            break;
+        default:
+            weight1 = 1;
+    }
+    return [weight1, weight2, weight3];
+};
+
+export const environmentWeights = (a6, a7, a8) => {
+    let [weight3, weight2, weight1] = [1, 1, 1];
+    switch (a6) {
+        case "0":
+            weight1 = 1;
+            break;
+        case "1":
+            weight1 = 2;
+            break;
+        case "2":
+            weight1 = 3;
+            break;
+        case "3":
+            weight1 = 5;
+            break;
+        default:
+            weight1 = 1;
+    }
+    switch (a7) {
+        case "0":
+            weight2 = 1;
+            break;
+        case "1":
+            weight2 = 3;
+            break;
+        case "2":
+            weight2 = 5;
+            break;
+        default:
+            weight2 = 1;
+    }
+    switch (a8) {
+        case "0":
+            weight1 = 1;
+            break;
+        case "1":
+            weight1 = 3;
+            break;
+        case "2":
+            weight1 = 5;
+            break;
+        default:
+            weight1 = 1;
+    }
+    return [weight1, weight2, weight3];
+};
