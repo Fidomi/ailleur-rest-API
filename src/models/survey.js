98a8e31a315b1927422925c40b15fba38880b8a5
Fidomivalua <sidonie.moulart@gmail.com>
2021-11-08 11:28:14 +0100

    Fix : translate french labels to english

--- src/models/survey.js
+++ src/models/survey.js
@@ -1,19 +1,19 @@
 const survey = [
     {
         question: "Situation familiale",
-        réponses: [
-            "vit seul sans enfant à charge",
-            "vit en couple avec enfant(s) à charge",
-            "vit en coupple sans enfant",
+        answers: [
+            "Seul sans enfant à charge",
+            "En couple avec enfant(s) à charge",
+            "En couple sans enfant",
         ],
     },
     {
         question: "tranche d'âge",
-        réponses: ["18-25 ans", "25-45 ans", "45-65 ans", "plus de 65 ans"],
+        answers: ["18-25 ans", "25-45 ans", "45-65 ans", "plus de 65 ans"],
     },
     {
         question: "part du télétravail dans votre activité",
-        réponses: [
+        answers: [
             "Aucune",
             "1 à 2 jours par semaine",
             "3 jours et plus par semaine",
@@ -21,7 +21,7 @@ const survey = [
     },
     {
         question: "Craignez-vous d'être cambriolé?",
-        réponses: [
+        answers: [
             "non, je n`y pense jamais",
             "crainte modérée",
             "sécuriser mon habitation est une de mes priorités",
@@ -30,7 +30,7 @@ const survey = [
     {
         question:
             "Combien de kilomètres êtes-vous prêt à parcourir pour aller voir votre généraliste?",
-        réponses: [
+        answers: [
             "jusqu'à 3km",
             "jusqu'a 10 km",
             "jusqu'a 25 km",
@@ -40,7 +40,7 @@ const survey = [
     {
         question:
             "Est-ce que le nombre d'agressions sur un territoire est un critère important pour vous?",
-        réponses: [
+        answers: [
             "non, je n'y pense jamais",
             "c'est un critère à prendre en compte",
             "c'est un critère primordial",
@@ -48,7 +48,7 @@ const survey = [
     },
     {
         question: "Quelle est la part du bio dans votre alimentation?",
-        réponses: [
+        answers: [
             "quelques produits, disons moins de 10% de mon alimentation",
             "je consomme régulièrement bio, disons environ 50% de mon alimentation",
             "j'achète bio autant que possible, disons 80% de mon alimentation",
@@ -57,7 +57,7 @@ const survey = [
     },
     {
         question: "Supportez-vous la chaleur?",
-        réponses: [
+        answers: [
             "je crains la chaleur et préfère la fraîcheur",
             "j'ai peur de la canicule et préfère quand il fait moins de 30 degrés",
             "je supporte bien la chaleur et adore le climat méditerrannéen",
@@ -66,11 +66,11 @@ const survey = [
     {
         question:
             "A quelle distance d'une centrale nucléaire seriez-vous prêt à vous installer?",
-        réponses: ["dès 0 km", "à plus de 50 km", "à plus de 100 km"],
+        answers: ["dès 0 km", "à plus de 50 km", "à plus de 100 km"],
     },
     // {
     //     question: "La pluie vous déprime-t-elle?",
-    //     réponses: [
+    //     answers: [
     //         "je ne suis pas en sucre, la pluie n'a jamais été un problème pour moi",
     //         "il faut bien qu'il pleuve un peu de temps en temps, mais pas trop non plus",
     //         "je déteste les jours de pluie",
