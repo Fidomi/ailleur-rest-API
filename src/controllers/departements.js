d4ec96157cdb704e4d857999145e8fdfa0907f91
Fidomi <sidonie.moulart@gmail.com>
2021-12-09 14:37:03 +0100

    change homepage for deployment

--- src/controllers/departements.js
+++ src/controllers/departements.js
@@ -1,9 +1,9 @@
 const departementData = require("../models/departements");
 
-const getDepartementWithId = (id) => {
-    return departementData.find(
+exports.getDepartementWithId = function (id) {
+    console.log("id", id);
+    const mydepartement = departementData.find(
         (departement) => departement.num_dep === id.toString()
     );
+    return mydepartement;
 };
-
-module.exports = getDepartementWithId;
--- src/routes/index.js
+++ src/routes/index.js
@@ -131,9 +131,25 @@ router.get(
 );
 
 router.get("/api", function (req, res, next) {
+    console.log("API /");
     res.render("index", { title: "Ailleurs's REST API" });
 });
 
-router.use("/*", express.static(path.resolve(__dirname, "../../client/build")));
+if (
+    process.env.NODE_ENV === "production" ||
+    process.env.NODE_ENV === "staging"
+) {
+    //app.use(express.static(path.join(__dirname, "client/build")));
+    // Have Node serve the files for our built React app
+    router.use(
+        "/*",
+        express.static(path.join(__dirname, "../../client/build"))
+    );
+}
+
+// All other GET requests not handled before will return our React app
+router.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "../../client/build", "index.html"));
+});
 
 module.exports = router;
